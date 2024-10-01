3d472086b2bc6fa6547d1b5e39f47dbb8ba76e62
Nodari L <->
2024-10-01 13:39:58 +0200

    ~ examples

--- examples/main.cc
+++ examples/main.cc
@@ -1,15 +1,21 @@
 #include <iostream>
 
 #include "db.h"
+#include "row.h"
 
 int main() {
-  norodb::DBOptions opts;
-  norodb::DB db("dbdir", opts);
+  // norodb::DBOptions opts;
+  // norodb::DB db("", opts);
+  // db.open();
+  //
+  // norodb::ByteArr key;
+  // norodb::ByteArr val;
+  //
+  // db.put(key, val);
 
-  norodb::ByteArr key;
-  norodb::ByteArr val;
-
-  db.put(key, val);
+  norodb::RowHeader rh(1,2,3,4,5);
+  auto buffer = rh.serialize();
+  std::cout << "Buffer size is " << buffer->size() << std::endl;
 
   std::cout << "Done!" << std::endl;
 }
