ad7532295483e43ba3ae505bba9bc998fc27702b
Nodari L <->
2024-10-01 13:39:59 +0200

    added rollover of the current data file

--- src/db.cc
+++ src/db.cc
@@ -2,19 +2,24 @@
 
 #include <iostream>
 
+#include "constants.h"
+
 namespace norodb {
 
 Status DB::open() {
   fs::path p("norodb");
-  DBDirectory db_dir(p);
-  data_file = new DBFile(0, db_dir, db_options);
+  db_dir = DBDirectory(p);
   return Status(true);
 }
 
 Status DB::put(ByteBuffer& key, ByteBuffer& val) {
   write_lock.lock();
+  seq_num++;
   Row row(&key, &val);
-  data_file->write_row(row);
+  row.set_seq_num(seq_num);
+  row.set_version(DATA_FILE_VERSION);
+  write_row(row);
+  // add row to the index
   write_lock.unlock();
   return Status(true);
 }
@@ -22,5 +27,22 @@ Status DB::put(ByteBuffer& key, ByteBuffer& val) {
 Status DB::get(const ByteBuffer& key) { return Status(true); }
 
 Status DB::remove(const ByteBuffer& key) { return Status(true); }
+
+void DB::write_row(Row& row) {
+  if (!curr_data_file or
+      curr_data_file->get_write_offset() + row.size() > db_options.MAX_FILE_SIZE) {
+    roll_over_current_data_file();
+  }
+
+  curr_data_file->write_row(row);
+}
+
+void DB::roll_over_current_data_file() {
+  if (curr_data_file) {
+    curr_data_file->flush();
+  }
+
+  curr_data_file = std::unique_ptr<DBFile>(new DBFile(get_next_file_id(), db_dir, db_options));
+}
 
 }  // namespace norodb
--- src/db.h
+++ src/db.h
@@ -1,8 +1,10 @@
 #ifndef NORO_DB_DB_H_
 #define NORO_DB_DB_H_
 
-#include <string>
+#include <atomic>
+#include <memory>
 #include <mutex>
+#include <string>
 
 #include "byte_buffer.h"
 #include "db_directory.h"
@@ -16,16 +18,51 @@ namespace norodb {
 class DB {
   std::string directory;
   DBOptions db_options;
-  DBFile* data_file;
+  DBDirectory db_dir;
+  std::unique_ptr<DBFile> curr_data_file;  // current data file
   std::mutex write_lock;
+  uint64_t seq_num = 0;  // sequence number
+  std::atomic<uint32_t> file_id = 0;
 
  public:
-  DB(const std::string& dir, const DBOptions& opts) : directory(dir), db_options(opts) {}
+  DB(const std::string& dir, const DBOptions& opts) : directory(dir), db_options(opts){};
   Status open();
 
+  /**
+   * Adds a key/valiue  pair to the db.
+   *
+   * @param key - a ByteBuffer that represents a key
+   * @param val - a ByteBuffer that represents a value
+   * @return status of the operation
+   */
   Status put(ByteBuffer& key, ByteBuffer& val);
+
+  /**
+   * Searches for a key represented by a ByteBuffer. If a value assosiated with the key
+   * is found it's returned, otherwise ... #TODO fix this doc
+   *
+   * @param key - a ByteBuffer that represents a key
+   */
   Status get(const ByteBuffer& key);
   Status remove(const ByteBuffer& key);
+
+  /**
+   * Writes a row to the current data file. If there is no open data file or the
+   * current data file file size will exceed the max file size (check DBOptions) then
+   * the current data file will be closed and a new data file will be opened
+   *
+   * @param row - a row to be written
+   */
+  void write_row(Row& row);
+  /**
+   * Flushes and closes the current data file and cretes a new one
+   */
+  void roll_over_current_data_file();
+
+  uint32_t get_next_file_id() {
+    file_id++;
+    return file_id;
+  }
 };
 }  // namespace norodb
 
