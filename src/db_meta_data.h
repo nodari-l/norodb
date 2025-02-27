8734ac9b3f2da761a749b8fa2fc48eb5a675d860
Nodari L <nodari.lipartiya@gmail.com>
2025-02-27 22:45:43 +0100

    started metadata file

---
+++ src/db_meta_data.h
@@ -0,0 +1,37 @@
+#ifndef NORO_DB_DB_META_DATA_H_
+#define NORO_DB_DB_META_DATA_H_
+
+#include "byte_buffer.h"
+#include "db_directory.h"
+#include "db_options.h"
+#include "db_file.h"
+
+namespace norodb {
+
+class DBMetaData : public DBFile {
+  DBDirectory db_dir;
+
+  uint64_t check_sum = 0;
+  uint8_t version = 0;
+  uint64_t seq_num = 0;
+  uint8_t max_file_size = 0;
+
+  static const int SIZE = 8 + 1 + 8 + 1;
+  static constexpr std::string METADATA_FILE_NAME = "metadata";
+
+public:
+  DBMetaData(uint32_t file_id, DBDirectory& dir, DBOptions& options);
+
+  /**
+   * Saves metadata to disk;
+   */
+  void save();
+
+  void load();
+
+};
+
+} // namespace nododb
+
+#endif
+
