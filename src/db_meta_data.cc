ce4100b0d02c9d66c25b4fedd1fc3835ade158af
Nodari L <2669197+nodari-l@users.noreply.github.com>
2025-02-27 22:45:43 +0100

    started metadata file

---
+++ src/db_meta_data.cc
@@ -0,0 +1,31 @@
+#include "db_meta_data.h"
+
+namespace norodb {
+
+
+DBMetaData::DBMetaData(uint32_t file_id, DBDirectory& dir, DBOptions& options)
+    : DBFile{file_id, dir, options} {
+
+  fs::path file_path = db_dir.get_path() / METADATA_FILE_NAME;
+
+  file.open(file_path.string(),
+            std::ios::in | std::ios::out | std::ios::app | std::ios::ate | std::ios::binary);
+}
+
+
+void DBMetaData::save() {
+  ByteBuffer buffer(SIZE);
+  buffer.put_long(check_sum);
+  buffer.put(version);
+  buffer.put_long(seq_num);
+  buffer.put(max_file_size);
+
+  write(buffer);
+}
+
+
+void load() {
+
+}
+
+}
